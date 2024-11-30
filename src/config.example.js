0ab1259b1a783bb13a9317da3d94cf78a24e160c
Sofian <sofian.developpeur@gmail.com>
2024-12-01 00:08:19 +0100

    add comments to config file

--- src/config.example.js
+++ src/config.example.js
@@ -1,15 +1,15 @@
 // Remove the ".example" from the name
 module.exports = {
     roles: {
-        msgPumpsId: "",
-        msgFuelId: "",
-        employeeRoleId: "",
-        pompisteRoleId: "",
-        salesRoleId: "",
-        salesNoviceRoleId: "",
+        msgPumpsId: "", // l'id du message de la gestion d'essence des pompes
+        msgFuelId: "", // l'id du message de l'affichage des prix de l'essence
+        employeeRoleId: "", // l'id du rôle employé
+        pompisteRoleId: "", // l'id du rôle pompiste
+        salesRoleId: "", // l'id du rôle vendeur
+        salesNoviceRoleId: "", // l'id du rôle vendeur novice
     },
     channels: {
-        recrutmentId: "",
+        recrutmentId: "", // l'id du salon recrutement
     },
     colors: {
         default: "#232959",
